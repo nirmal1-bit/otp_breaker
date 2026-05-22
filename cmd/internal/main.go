8b8f8c0e1260461827126288316ea2036eddaa5b
batman <darwinkoirala123@gmail.com>
2026-05-22 08:16:37 -0600

    fix: use same tcp conn over muntiple req

--- cmd/internal/main.go
+++ cmd/internal/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
 	"sync"
 )
@@ -13,37 +14,47 @@ type body struct {
 }
 
 type app struct {
-	logger  *slog.Logger
-	url     string
-	body    body
-	method  string
-	workers int
-	length  int
+	logger       *slog.Logger
+	url          string
+	body         body
+	method       string
+	workers      int
+	length       int
+	stopHttpCode int
+	client       *http.Client //to reuse the same client over muntiple req for keep alive and not a lot off ssl handshake
 }
 
 func main() {
 
-	otp := make(chan string, 100) // make it buffered so it is decoupled from the workers
-
-	var wg sync.WaitGroup // assigning it's to it's type zero value
+	maxIdealCons := 300
+	wg := sync.WaitGroup{}
 
 	var app app
+	app.client = &http.Client{
+		Transport: &http.Transport{
+			MaxIdleConns: maxIdealCons,
+		},
+	}
+
 	flag.StringVar(&app.url, "url", "none", "The url")
 	flag.StringVar(&app.method, "method", "POST", "The method to be using")
 	flag.IntVar(&app.workers, "t", 10, "The threads to be used")
 	flag.IntVar(&app.length, "l", 4, "The length of the otp")
+	flag.IntVar(&app.stopHttpCode, "s", 200, "Enter the match http status code for matched otp")
 	flag.Parse()
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	app.logger = logger
 
-	fmt.Printf("Working with %d many threads", app.workers)
+	otp := make(chan string, app.workers*3)
 
-	for i := 0; i < app.workers; i++ {
+	fmt.Printf("Working with %d many gouroutine", app.workers)
+
+	for i := range app.workers {
 		wg.Add(1)
 		go app.callRequest(i, otp, &wg)
 	}
 
-	go app.generateOtp(otp)
+	app.generateOtp(otp) //if you run this as gorotine add wg.Add(1) for this too
 
 	wg.Wait()
 }
